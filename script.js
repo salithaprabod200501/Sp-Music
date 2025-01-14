f793d9d48a57b11189bf2d5093241b140b6f26e8
salithaprabod200501 <prabodsalitha58@gmail.com>
2025-01-15 00:02:50 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //1st song
 function downloadSongPerabhawesong() {
     // සින්දු ගොනුවේ සබැඳිය
-    const songUrl = 'songs/1.mp3';  // ඔබේ සින්දු ගොනුවේ සබැඳිය මෙහි දමන්න
+    const songUrl = 'Malwaram _ Raini Charuka - R_B Cover by Thirathi Amoda(MP3_160K).mp3';  // ඔබේ සින්දු ගොනුවේ සබැඳිය මෙහි දමන්න
     
     // temporary <a> link එකක් සාදන්න
 
